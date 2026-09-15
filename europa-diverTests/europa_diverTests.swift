a60e1416b499d69262b33803c83e2fb1608520ba
DylanBozarth <dylanbozarth@theluxergroup.com>
2026-09-15 12:24:29 -0700

    base porject

---
+++ europa-diverTests/europa_diverTests.swift
@@ -0,0 +1,17 @@
+//
+//  europa_diverTests.swift
+//  europa-diverTests
+//
+//  Created by Dylan Bozarth on 9/15/26.
+//
+
+import Testing
+@testable import europa_diver
+
+struct europa_diverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
